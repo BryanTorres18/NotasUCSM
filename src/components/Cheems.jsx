678fedef7d4585b4ed898b8326403ee6b5fb6d92
BryanTorres18 <90003001+BryanTorres18@users.noreply.github.com>
2024-01-18 17:05:11 -0500

    segundo

--- src/components/Cheems.jsx
+++ src/components/Cheems.jsx
@@ -6,10 +6,10 @@ function Cheems() {
 
   return (
     <div className="grid justify-items-center">
-      <p className="bg-blanco p-2 rounded-md mb-4 text-3xl font-bold">
+      <p className="bg-blanco p-2 rounded-md mb-4 text-3xl font-bold md:text-lg">
         {currentImage.mensaje}
       </p>
-      <div className="h-96 w-96 min-h-20 min-w-20 
+      <div className="h-80 w-80 md:h-96 md:w-96 min-h-15 min-w-15 
           bg-negro rounded-xl overflow-hidden">
         <img 
           className="object-cover w-full h-full"
